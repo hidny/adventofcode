9febbb92af30c8ae930457d69b1c6dceb58aa1b3
hidny <mtardibuono@gmail.com>
2017-12-06 00:58:42 -0500

    Day 6: lead given away! Read better next time

--- src/probs2017/prob5.java
+++ src/probs2017/prob5.java
@@ -55,3 +55,21 @@ public class prob5 {
 	
 
 }
+
+/*//Mike Harris' solution in python:
+//It's so much smaller. So much less typing!
+a = open('input.txt', 'r').readlines()
+b = map(int, a)
+i = 0
+count = 0
+
+while 1:
+	if i >= len(b) or i < 0:
+		break
+	count += 1
+	n = i + b[i]
+	b[i] += 1
+	i = n
+	
+print count	
+*/
--- src/probs2017/prob6.java
+++ src/probs2017/prob6.java
@@ -13,12 +13,53 @@ public class prob6 {
 			 in = new Scanner(new File("in2017/prob2017in6.txt"));
 			
 			int count = 0;
-			boolean part2 = true;
+			boolean part2 = false;
 			String line = "";
+			ArrayList <String>lines = new ArrayList<String>();
 			
 			while(in.hasNextLine()) {
 				line = in.nextLine();
+				String token[] = line.split("\t");
+				
+				lines.add(line);
+				
+
+				//100000 is big enough... :)
+				int number[][] = new int[100000][token.length];
+				
+				for(int i=0; i<token.length; i++) {
+					number[0][i] = Integer.parseInt(token[i]);
+				}
+				boolean noMatch = true;
+				while(noMatch) {
+					count++;
+					
+					int maxIndex = 0;
+					
+					for(int i=1; i<token.length; i++) {
+						System.out.print(number[count-1][i] + "\t");
+						if(number[count-1][i] > number[count-1][maxIndex]) {
+							maxIndex = i;
+						}
+					}
+					System.out.println();
+					int num = number[count-1][maxIndex];
+					
+					
+					for(int i=0; i<token.length; i++) {
+						number[count][i] = number[count-1][i];
+					}
+					number[count][maxIndex] = 0;
+					for(int i=0; i<num; i++) {
+						number[count][(maxIndex + 1 + i )% token.length]++;
+					}
+					
+					if(checkMatch(number, count, part2)) {
+						break;
+					}
+				}
 			}
+			
 			
 			System.out.println("Answer: " + count);
 			in.close();
@@ -27,6 +68,30 @@ public class prob6 {
 			e.printStackTrace();
 		} finally {
 		}
+	}
+	
+	public static boolean checkMatch(int number[][], int count, boolean part2) {
+		
+		for(int i=0; i<count-1; i++) {
+			boolean good = true;
+			for(int j=0; j<number[0].length; j++) {
+				if(number[i][j] != number[count][j]) {
+					good = false;
+					break;
+				}
+				
+			}
+			
+			if(good) { 
+				if(part2) {
+					System.out.println(count - i);
+					System.exit(1);
+				}
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 
