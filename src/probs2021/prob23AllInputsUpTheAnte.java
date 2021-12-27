20fe2927bcdc074cd6a8b4a7a5fdc235f2e9d7ab
hidny <mtardibuono@gmail.com>
2021-12-27 17:36:11 -0500

    Solved day 23 for every permutation

--- src/probs2021/prob23AllInputsUpTheAnte.java
+++ src/probs2021/prob23AllInputsUpTheAnte.java
@@ -9,19 +9,21 @@ import number.IsNumber;
 
 public class prob23AllInputsUpTheAnte {
 
-
-
+//Challenge:
+//https://www.reddit.com/r/adventofcode/comments/rmryir/2021_day_23_part_12_solve_for_all_possible_start/
+//Computer can figure it out in 10 minutes!
 	
 	//TODO: setup main.
 	public static void main(String args[]) {
 		
-		String puzzle = "...........CDDBACBABBADDACC";
+		String initPuzzle = "...........ADDABCBBCBACDACD";
+		//String puzzle = "...........CDDBACBABBADDACC";
 		//String puzzle = "AB..........AAA.BBBCCCCDDDD";
 		
-		prob23UtilFunctions.DEPTH_HOLE = (puzzle.length() - prob23UtilFunctions.LENGTH_UP) / prob23UtilFunctions.NUM_LETTERS;
+		prob23UtilFunctions.DEPTH_HOLE = (initPuzzle.length() - prob23UtilFunctions.LENGTH_UP) / prob23UtilFunctions.NUM_LETTERS;
 		
 		
-		prob23UtilFunctions.GOAL_INT = new int[puzzle.length()];
+		prob23UtilFunctions.GOAL_INT = new int[initPuzzle.length()];
 		
 		for(int i=0; i<prob23UtilFunctions.LENGTH_UP; i++) {
 			prob23UtilFunctions.GOAL_INT[i] = prob23PosbEvenFaster.EMPTY;
@@ -41,139 +43,235 @@ public class prob23AllInputsUpTheAnte {
 		prob23UtilFunctions.setupPathLengthsAndPaths();
 		
 		
-
-		int start[] = prob23PosbEvenFaster.getIntArray(puzzle);
-
 		
 		prob23PosbEvenFaster.setupAdjustments(prob23UtilFunctions.GOAL_INT);
 
-		BigInteger initialCode = prob23PosbEvenFaster.getCodeFromScratch(start);
-		
 		BigInteger finalCode = prob23PosbEvenFaster.getCodeFromScratch(prob23UtilFunctions.GOAL_INT);
 		
-		prob23PosbEvenFaster pos = new prob23PosbEvenFaster(initialCode, start);
-		
-		sopl("Map:");
-		sopl(pos.getMap());
-
+	
 		prob23PosbEvenFaster finalMap = new prob23PosbEvenFaster(finalCode, prob23UtilFunctions.GOAL_INT);
 		sopl("Final map:");
 		sopl(finalMap.getMap());
+
+		
+		int maxEnergy = 0;
+		int sumEnergy = 0;
 		
 		
-		sopl(pos.getAdmissibleHeuristic(null));
+		String initArray[] = getInitialPermArray(initPuzzle);
 		
-		pos.getNodeNeighbours();
+		//
 		
+		//***************************
 		
+		//Check perm valid (1 before 2)
 		
-		ArrayList<AstarNode> path = AstarAlgo.astar(pos, null);
+		//create puzzle string
 		
-		sopl("-------------------------");
-		sopl("Path: " + path.size());
-		
-		int answer = 0;
-		for(int i=0; i<path.size() - 1; i++) {
-			answer += path.get(i).getCostOfMove(path.get(i+1));
+		int curNumToSkip = 0;
+		/*
+		sopl("Print perms:");
+		for(int perm=0; perm<utilsPE.Permutation.getSmallFactorial(8); perm += curNumToSkip) {
 			
-			if(i== 0) {
-				sopl(((prob23PosbEvenFaster)path.get(i)).getMap());
-				
+			String permTable[] = utilsPE.Permutation.generatePermutation(initArray, perm);
+			
+			
+			
+			int firstIndex = getIndexFirstInvalidPair(permTable);
+			
+			if(firstIndex >= 0) {
+				curNumToSkip = (int)utilsPE.Permutation.getSmallFactorial(8 - firstIndex - 1);
+				continue;
 			} else {
-				//I just want to make the solution look good:
-				sopl(((prob23PosbEvenFaster)path.get(i)).getMapWithMovement((prob23PosbEvenFaster)path.get(i-1)));
+				curNumToSkip = 1;
 			}
 			
-			sopl(path.get(i).getCostOfMove(path.get(i+1)));
+			for(int i=0; i<permTable.length; i++) {
+				sop(permTable[i] + ", ");
+			}
+			sopl();
+		}
+		exit(0);
+		*/
+		
+		
+		int debugNumIterations = 0;
+		int numFailed = 0;
+		
+		for(int perm=0; perm<utilsPE.Permutation.getSmallFactorial(8); perm += curNumToSkip) {
+			
+			String permTable[] = utilsPE.Permutation.generatePermutation(initArray, perm);
+			
+			int firstIndex = getIndexFirstInvalidPair(permTable);
+			
+			if(firstIndex >= 0) {
+				curNumToSkip = (int)utilsPE.Permutation.getSmallFactorial(8 - firstIndex - 1);
+				continue;
+			} else {
+				curNumToSkip = 1;
+			}
+			
+			String curPuzzle = setupPuzzle(initPuzzle, permTable);
+			//TODO: check if perm valid...
+			
+			int start[] = prob23PosbEvenFaster.getIntArray(curPuzzle);
+			
+			BigInteger initialCode = prob23PosbEvenFaster.getCodeFromScratch(start);
+			
+			prob23PosbEvenFaster pos = new prob23PosbEvenFaster(initialCode, start);
+	
+			//sopl("Map:");
+			//sopl(pos.getMap());
+			
+			sopl(pos.getAdmissibleHeuristic(null));
+			
+			//sopl("First call to neighbours:");
+			//pos.getNodeNeighbours();
+			//sopl("End first call to neighbours");
+			
+			
+			ArrayList<AstarNode> path = AstarAlgo.astar(pos, null);
+
+			prob23PosbEvenFaster.refreshMap();
+			
+			sopl("-------------------------");
+			if(path != null) {
+				sopl("Path: " + path.size());
+				
+				int answer = 0;
+				for(int i=0; i<path.size() - 1; i++) {
+					answer += path.get(i).getCostOfMove(path.get(i+1));
+					
+					if(i== 0) {
+						sopl("Start:");
+						sopl(((prob23PosbEvenFaster)path.get(i)).getMap());
+						
+					} else {
+						//I just want to make the solution look good:
+						//sopl(((prob23PosbEvenFaster)path.get(i)).getMapWithMovement((prob23PosbEvenFaster)path.get(i-1)));
+					}
+					
+					//sopl(path.get(i).getCostOfMove(path.get(i+1)));
+				}
+				
+				//Print the last position, just for fun:
+				//sopl(((prob23PosbEvenFaster)path.get(path.size() - 1)).getMapWithMovement((prob23PosbEvenFaster)path.get(path.size() - 2)));
+				
+				sopl("Answer: " + answer);
+				
+				
+				if(maxEnergy < answer) {
+					maxEnergy = answer;
+				} else if(maxEnergy == answer) {
+					sopl("DEBUG: Max energy tied at: " + maxEnergy);
+				}
+				
+				sumEnergy += answer;
+			} else {
+				numFailed++;
+				
+				sopl("Failed position:");
+				sopl(pos.getMap());
+				sopl("Num failed: " + numFailed);
+				
+			}
+		//*****************************
+
+			debugNumIterations++;
+			sopl("Num iterations: " + debugNumIterations);
 		}
 		
-		//Print the last position, just for fun:
-		sopl(((prob23PosbEvenFaster)path.get(path.size() - 1)).getMapWithMovement((prob23PosbEvenFaster)path.get(path.size() - 2)));
-		
-		sopl("Answer: " + answer);
+		sopl("Max energy: " + maxEnergy);
+		sopl("Sum energy: " + sumEnergy);
 	}
 	
 	
-
-	//TODO: setup main.
-	public static void mainOld(String args[]) {
+	public static String[] getInitialPermArray(String initPuzzle) {
 		
-		String puzzle = "...........CDDBACBABBADDACC";
-		//String puzzle = "AB..........AAA.BBBCCCCDDDD";
+		String ret[] = new String[8];
 		
-		prob23UtilFunctions.DEPTH_HOLE = (puzzle.length() - prob23UtilFunctions.LENGTH_UP) / prob23UtilFunctions.NUM_LETTERS;
+		int curAddIndex = 0;
 		
-		
-		prob23UtilFunctions.GOAL_INT = new int[puzzle.length()];
-		
-		for(int i=0; i<prob23UtilFunctions.LENGTH_UP; i++) {
-			prob23UtilFunctions.GOAL_INT[i] = prob23PosbEvenFaster.EMPTY;
-		}
-		
-		for(int i=0; i<4; i++) {
-			for(int j=0; j<prob23UtilFunctions.DEPTH_HOLE; j++) {
-				prob23UtilFunctions.GOAL_INT[prob23UtilFunctions.LENGTH_UP + prob23UtilFunctions.DEPTH_HOLE * i + j] = (i+1);
+		for(int i=0; i<initPuzzle.length(); i++) {
+			
+			if( i < prob23UtilFunctions.LENGTH_UP) {
+				continue;
+			}
+			
+			if((i - prob23UtilFunctions.LENGTH_UP) % prob23UtilFunctions.DEPTH_HOLE == 0
+					|| (i - prob23UtilFunctions.LENGTH_UP) % prob23UtilFunctions.DEPTH_HOLE == prob23UtilFunctions.DEPTH_HOLE - 1) {
+				ret[curAddIndex] = initPuzzle.charAt(i) + "";
+				curAddIndex++;
 			}
 		}
 		
+		if(curAddIndex != 8) {
+			sopl("Oops!");
+			exit(1);
+		}
+		boolean seen[] = new boolean[4];
 		
-		prob23UtilFunctions.neighbours = new int[prob23UtilFunctions.GOAL_INT.length][prob23UtilFunctions.GOAL_INT.length];
-		prob23UtilFunctions.setupNeighbours();
-		
-		prob23UtilFunctions.pathLengths = new int[prob23UtilFunctions.GOAL_INT.length][prob23UtilFunctions.GOAL_INT.length];
-		prob23UtilFunctions.setupPathLengthsAndPaths();
-		
-		
-
-		int start[] = prob23PosbEvenFaster.getIntArray(puzzle);
-
-		
-		prob23PosbEvenFaster.setupAdjustments(prob23UtilFunctions.GOAL_INT);
-
-		BigInteger initialCode = prob23PosbEvenFaster.getCodeFromScratch(start);
-		
-		BigInteger finalCode = prob23PosbEvenFaster.getCodeFromScratch(prob23UtilFunctions.GOAL_INT);
-		
-		prob23PosbEvenFaster pos = new prob23PosbEvenFaster(initialCode, start);
-		
-		sopl("Map:");
-		sopl(pos.getMap());
-
-		prob23PosbEvenFaster finalMap = new prob23PosbEvenFaster(finalCode, prob23UtilFunctions.GOAL_INT);
-		sopl("Final map:");
-		sopl(finalMap.getMap());
-		
-		
-		sopl(pos.getAdmissibleHeuristic(null));
-		
-		pos.getNodeNeighbours();
-		
-		
-		
-		ArrayList<AstarNode> path = AstarAlgo.astar(pos, null);
-		
-		sopl("-------------------------");
-		sopl("Path: " + path.size());
-		
-		int answer = 0;
-		for(int i=0; i<path.size() - 1; i++) {
-			answer += path.get(i).getCostOfMove(path.get(i+1));
+		for(int i=0; i<ret.length; i++) {
 			
-			if(i== 0) {
-				sopl(((prob23PosbEvenFaster)path.get(i)).getMap());
-				
+			int seenIndex = (int)(ret[i].charAt(0) - 'A');
+			
+			if(seen[seenIndex] == false) {
+				ret[i] = ret[i] + "1";
+				seen[seenIndex] = true;
 			} else {
-				//I just want to make the solution look good:
-				sopl(((prob23PosbEvenFaster)path.get(i)).getMapWithMovement((prob23PosbEvenFaster)path.get(i-1)));
+				ret[i] = ret[i] + "2";
 			}
 			
-			sopl(path.get(i).getCostOfMove(path.get(i+1)));
+			sop(ret[i] + ", ");
+		}
+		sopl();
+		
+		return ret;
+		
+	}
+	
+	public static int getIndexFirstInvalidPair(String permTable[]) {
+		
+		boolean seen[] = new boolean[4];
+		
+		for(int i=0; i<permTable.length; i++) {
+
+			int seenIndex = (int)(permTable[i].charAt(0) - 'A');
+			
+			
+			if(seen[seenIndex] == false) {
+				seen[seenIndex] = true;
+				
+				if((int)(permTable[i].charAt(1) - '0') > 1) {
+					return i;
+				}
+			}
 		}
 		
-		//Print the last position, just for fun:
-		sopl(((prob23PosbEvenFaster)path.get(path.size() - 1)).getMapWithMovement((prob23PosbEvenFaster)path.get(path.size() - 2)));
+		return -1;
 		
-		sopl("Answer: " + answer);
+	}
+	
+	public static String setupPuzzle(String initialPuzzle, String perm[]) {
+		
+		String ret = "";
+		
+		int permIndexToUse = 0;
+		
+		for(int i=0; i<initialPuzzle.length(); i++) {
+			if(i < prob23UtilFunctions.LENGTH_UP) {
+				ret += initialPuzzle.charAt(i) + "";
+			
+			} else if( (i - prob23UtilFunctions.LENGTH_UP) % prob23UtilFunctions.DEPTH_HOLE == 0
+					|| (i - prob23UtilFunctions.LENGTH_UP) % prob23UtilFunctions.DEPTH_HOLE == prob23UtilFunctions.DEPTH_HOLE - 1) {
+				ret += perm[permIndexToUse].charAt(0) + "";
+				permIndexToUse++;
+			} else {
+				ret += initialPuzzle.charAt(i) + "";
+			}
+		}
+		
+		return ret;
 	}
 	
 	
--- src/probs2021/prob23PosbEvenFaster.java
+++ src/probs2021/prob23PosbEvenFaster.java
@@ -16,6 +16,88 @@ import utils.Sort;
 
 public class prob23PosbEvenFaster implements AstarNode {
 
+
+
+	//TODO: setup main.
+	public static void main(String args[]) {
+		
+		String puzzle = "...........CDDBACBABBADDACC";
+		//String puzzle = "AB..........AAA.BBBCCCCDDDD";
+		
+		prob23UtilFunctions.DEPTH_HOLE = (puzzle.length() - prob23UtilFunctions.LENGTH_UP) / prob23UtilFunctions.NUM_LETTERS;
+		
+		
+		prob23UtilFunctions.GOAL_INT = new int[puzzle.length()];
+		
+		for(int i=0; i<prob23UtilFunctions.LENGTH_UP; i++) {
+			prob23UtilFunctions.GOAL_INT[i] = prob23PosbEvenFaster.EMPTY;
+		}
+		
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<prob23UtilFunctions.DEPTH_HOLE; j++) {
+				prob23UtilFunctions.GOAL_INT[prob23UtilFunctions.LENGTH_UP + prob23UtilFunctions.DEPTH_HOLE * i + j] = (i+1);
+			}
+		}
+		
+		
+		prob23UtilFunctions.neighbours = new int[prob23UtilFunctions.GOAL_INT.length][prob23UtilFunctions.GOAL_INT.length];
+		prob23UtilFunctions.setupNeighbours();
+		
+		prob23UtilFunctions.pathLengths = new int[prob23UtilFunctions.GOAL_INT.length][prob23UtilFunctions.GOAL_INT.length];
+		prob23UtilFunctions.setupPathLengthsAndPaths();
+		
+		
+
+		int start[] = prob23PosbEvenFaster.getIntArray(puzzle);
+
+		
+		prob23PosbEvenFaster.setupAdjustments(prob23UtilFunctions.GOAL_INT);
+
+		BigInteger initialCode = prob23PosbEvenFaster.getCodeFromScratch(start);
+		
+		BigInteger finalCode = prob23PosbEvenFaster.getCodeFromScratch(prob23UtilFunctions.GOAL_INT);
+		
+		prob23PosbEvenFaster pos = new prob23PosbEvenFaster(initialCode, start);
+		
+		sopl("Map:");
+		sopl(pos.getMap());
+
+		prob23PosbEvenFaster finalMap = new prob23PosbEvenFaster(finalCode, prob23UtilFunctions.GOAL_INT);
+		sopl("Final map:");
+		sopl(finalMap.getMap());
+		
+		
+		sopl(pos.getAdmissibleHeuristic(null));
+		
+		pos.getNodeNeighbours();
+		
+		
+		
+		ArrayList<AstarNode> path = AstarAlgo.astar(pos, null);
+		
+		sopl("-------------------------");
+		sopl("Path: " + path.size());
+		
+		int answer = 0;
+		for(int i=0; i<path.size() - 1; i++) {
+			answer += path.get(i).getCostOfMove(path.get(i+1));
+			
+			if(i== 0) {
+				sopl(((prob23PosbEvenFaster)path.get(i)).getMap());
+				
+			} else {
+				//I just want to make the solution look good:
+				sopl(((prob23PosbEvenFaster)path.get(i)).getMapWithMovement((prob23PosbEvenFaster)path.get(i-1)));
+			}
+			
+			sopl(path.get(i).getCostOfMove(path.get(i+1)));
+		}
+		
+		//Print the last position, just for fun:
+		sopl(((prob23PosbEvenFaster)path.get(path.size() - 1)).getMapWithMovement((prob23PosbEvenFaster)path.get(path.size() - 2)));
+		
+		sopl("Answer: " + answer);
+	}
 	
 
 	public static int LENGTH_UP = 11;
@@ -24,6 +106,9 @@ public class prob23PosbEvenFaster implements AstarNode {
 
 	private static Hashtable<BigInteger, prob23PosbEvenFaster> map = new Hashtable<BigInteger, prob23PosbEvenFaster>();
 
+	public static void refreshMap() {
+		map = new Hashtable<BigInteger, prob23PosbEvenFaster>();
+	}
 
 	private BigInteger curPos;
 	private int pos[];
@@ -465,7 +550,7 @@ public class prob23PosbEvenFaster implements AstarNode {
 			//END TESTING
 			count++;
 			
-			if(count % 1000 == 0) {
+			if(count % 100000 == 0) {
 				sopl("Neighbour 1:");
 				sopl("Count: " + count);
 				sopl("Cur:\n" + initialPos.getMap());
