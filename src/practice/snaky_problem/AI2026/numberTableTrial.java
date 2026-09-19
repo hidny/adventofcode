9485e028ddaf7224242bbaeae45c3a75eaffc781
Michael Tardibuono <mtardibuono@gmail.com>
2026-09-19 01:22:11 -0400

    I think it works, but it's confusing

--- src/practice/snaky_problem/AI2026/numberTableTrial.java
+++ src/practice/snaky_problem/AI2026/numberTableTrial.java
@@ -2,28 +2,33 @@ package practice.snaky_problem.AI2026;
 
 public class numberTableTrial {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public static int NUM_PLAYERS = 2;
-	public static int GRID_SIZE = 30;
-	public static int table[][][] = new int[NUM_PLAYERS][GRID_SIZE][GRID_SIZE];
+	public static final int NUM_PLAYERS = 2;
+	public static final int GRID_SIZE = 30;
+	public static final int table[][][] = new int[NUM_PLAYERS][GRID_SIZE][GRID_SIZE];
 	
-	public static boolean snaky_table[][] = {{false, true, true, true, true},
-							                 {true, true, false, false, false}};
+	public static final boolean snaky_table[][] = {{false, true, true, true, true},
+							                     {true, true, false, false, false}};
+	
+	public static final int snaky_length = snaky_table[0].length;
 
-	public static int ROTATIONS = 4;
-	public static int REFLECTIONS = 2;
-	public static int ORIENTATION = REFLECTIONS * ROTATIONS;
+	public static final int NUM_ROTATIONS = 4;
+	public static final int NUM_REFLECTIONS = 2;
+	public static final int NUM_ORIENTATION = NUM_REFLECTIONS * NUM_ROTATIONS;
+
+
+	public static boolean snaky_table_all_sym[][][] = new boolean[NUM_ORIENTATION][][];
+
+	//public static boolean relativeEffectTable[][][] = new boolean[ORIENTATION][snaky_length][snaky_length];
+	
+	
+	public static int numTreats[][][][] = new int[NUM_PLAYERS][NUM_ORIENTATION][GRID_SIZE][GRID_SIZE];
+	
 	static {
-		boolean snaky_table_all_sym[][][] = new boolean[ORIENTATION][][];
 		
 		int index = 0;
 		
-		for(int re=0; re<REFLECTIONS; re++) {
-			for(int r=0; r<ROTATIONS; r++) {
+		for(int re=0; re<NUM_REFLECTIONS; re++) {
+			for(int r=0; r<NUM_ROTATIONS; r++) {
 				
 				if(r % 2 == 1) {
 					
@@ -113,6 +118,133 @@ public class numberTableTrial {
 				System.out.println();
 			}
 			
+			System.out.println();
+		}
+		
+		/*
+		for(int n=0; n<snaky_table_all_sym.length; n++) {
+			
+			for(int i=0; i<snaky_table_all_sym[n].length; i++) {
+
+				for(int j=0; j<snaky_table_all_sym[n][i].length; j++) {
+					
+					//relativeEffectTable[n][i][j]
+					
+					if(snaky_table_all_sym[n][i][j]) {
+						
+						relativeEffectTable
+								[n]
+								[snaky_table_all_sym[n].length - 1 - i]
+								[snaky_table_all_sym[n][0].length - 1 - j] = true;
+					}
+					
+				}
+			}
+		}
+		
+		
+		System.out.println("Print relative thing...");
+		//Print relative and test it:
+		for(int n=0; n<relativeEffectTable.length; n++) {
+			
+			for(int i=0; i<relativeEffectTable[n].length; i++) {
+
+				for(int j=0; j<relativeEffectTable[n][i].length; j++) {
+					
+					if(relativeEffectTable[n][i][j]) {
+						System.out.print("#");
+					} else {
+						System.out.print("_");
+					}
+				}
+				System.out.println();
+			}
+			
+			System.out.println();
+		}*/
+	}
+	
+	public static void adjustTreatsAfterAdd(boolean isPlayer0, int i, int j) {
+		adjustTreats(isPlayer0, i, j, true);
+	}
+
+	public static void adjustTreatsAfterRemove(boolean isPlayer0, int i, int j) {
+		adjustTreats(isPlayer0, i, j, false);
+	}
+	
+	public static void adjustTreats(boolean isPlayer0, int i, int j, boolean dirAdd) {
+		
+		
+		if( ! (0<=i && i<GRID_SIZE && 0<=j && j<GRID_SIZE) ) {
+			System.out.println("Oops!");
+			return;
+		}
+		
+		int indexPlayer = isPlayer0 ? 0 : 1;
+		int dirTreat = dirAdd ? 1 : -1;
+		
+		for(int n=0; n<snaky_table_all_sym.length; n++) {
+			
+			for(int i2=0; i2<snaky_table_all_sym[n].length && i + i2 < GRID_SIZE; i2++) {
+
+				for(int j2=0; j2<snaky_table_all_sym[n][i2].length && j + j2 < GRID_SIZE; j2++) {
+					
+					if(snaky_table_all_sym[n][i2][j2]) {
+						
+						numTreats[indexPlayer][n][i+i2][j+j2] += dirTreat;
+						
+					}
+				}
+			}
+			
+		}
+		
+	}
+	
+	public static void main(String args[]) {
+		
+		adjustTreatsAfterAdd(true, 5, 5);
+		adjustTreatsAfterAdd(true, 5, 6);
+
+		adjustTreatsAfterAdd(true, 4, 6);
+		adjustTreatsAfterAdd(true, 4, 7);
+		adjustTreatsAfterAdd(true, 4, 8);
+		adjustTreatsAfterAdd(true, 4, 9);
+		
+		System.out.println("----------------------------");
+		boolean foundIt = false;
+		for(int n=0; n<NUM_ORIENTATION; n++) {
+			
+			for(int i=0; i<snaky_table_all_sym[n].length; i++) {
+
+				for(int j=0; j<snaky_table_all_sym[n][i].length; j++) {
+					
+					if(snaky_table_all_sym[n][i][j]) {
+						System.out.print("#");
+					} else {
+						System.out.print("_");
+					}
+				}
+				System.out.println();
+			}
+			
+			System.out.println();
+			
+			foundIt = false;
+			for(int i=0; i<GRID_SIZE; i++) {
+				for(int j=0; j<GRID_SIZE; j++) {
+					System.out.print(numTreats[0][n][i][j]);
+					
+					if(numTreats[0][n][i][j] == 6) {
+						foundIt = true;
+					}
+				}
+				System.out.println();
+			}
+			if(foundIt) {
+				System.out.println("found it!");
+			}
+			System.out.println();
 			System.out.println();
 		}
 	}
